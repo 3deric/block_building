cecc31ba1843b96a647d7467a3ffc3d00d6c19f5
Eric Schubert <eric.schubert@pixablestudios.com>
2018-08-20 19:42:02 +0200

    Added Cursor

---
+++ Assets/Scripts/cursorMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorMovement : MonoBehaviour {
+
+
+	RaycastHit hit;
+	Ray ray;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if(Physics.Raycast(ray, out hit) && hit.collider.tag == "gridElement")
+		{
+			this.transform.position = hit.collider.transform.position;
+		}
+	}
+}
